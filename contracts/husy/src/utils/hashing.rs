db403e5e965bf02899f0308c44ae1a2a65cc6f7c
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2022-04-26 15:50:16 +0200

    fefactor utils

---
+++ contracts/husy/src/utils/hashing.rs
@@ -0,0 +1,7 @@
+use near_sdk::{AccountId, CryptoHash, env};
+
+pub(crate) fn hash_account_id(account_id: &AccountId) -> CryptoHash {
+    let mut hash = CryptoHash::default();
+    hash.copy_from_slice(&env::sha256(account_id.as_bytes()));
+    hash
+}

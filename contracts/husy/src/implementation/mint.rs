de09fbd350b2be718dbf3313d10b5b35ea098600
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2022-04-27 20:46:01 +0200

    add nft_mint tests

--- contracts/husy/src/implementation/mint.rs
+++ contracts/husy/src/implementation/mint.rs
@@ -28,3 +28,97 @@ impl MintNFT for HusyContract {
         });
     }
 }
+
+#[cfg(test)]
+mod test {
+    use near_sdk::{env, testing_env, VMContext};
+    use near_sdk::{Balance, MockedBlockchain};
+
+    use crate::interface::ContractInit;
+
+    use super::*;
+
+    fn get_context(
+        predecessor_account_id: String,
+        storage_usage: u64,
+        attached: Balance,
+    ) -> VMContext {
+        VMContext {
+            current_account_id: "current.testnet".to_string(),
+            signer_account_id: "signer.testnet".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id,
+            input: vec![],
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage,
+            attached_deposit: attached,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view: false,
+            output_data_receivers: vec![],
+            epoch_height: 19,
+        }
+    }
+
+    #[test]
+    fn success_nft_mint() {
+        let attached = 999999999999999999999999999;
+        let context = get_context("aaa.testnet".to_string(), 10000000, attached);
+        testing_env!(context);
+        let mut contract = HusyContract::new_default("aaa.testnet".to_string());
+
+        let token_id = "token.testnet".to_string();
+        let metadata = MemeTokenMetadata {
+            title: Some("title".to_string()),
+            description: Some("description".to_string()),
+            ..Default::default()
+        };
+        let receiver_id = "receiver.testnet".to_string();
+
+        contract.nft_mint(token_id.clone(), metadata.clone(), receiver_id.clone());
+
+        assert_eq!(
+            contract.memes_by_id.get(&token_id),
+            Some(MemeToken {
+                owner_id: receiver_id.clone(),
+            })
+        );
+        assert_eq!(
+            contract.meme_metadata_by_id.get(&token_id),
+            Some(metadata.clone())
+        );
+        assert_eq!(
+            contract
+                .memes_per_owner
+                .get(&receiver_id.clone())
+                .unwrap()
+                .as_vector()
+                .get(0)
+                .unwrap(),
+            token_id.clone()
+        )
+    }
+
+    #[test]
+    #[should_panic]
+    fn token_already_exist() {
+        let attached = 9999999999999999999999999999;
+        let context = get_context("aaa.testnet".to_string(), 10000000, attached);
+        testing_env!(context);
+        let mut contract = HusyContract::new_default("aaa.testnet".to_string());
+
+        let token_id = "token.testnet".to_string();
+        let metadata = MemeTokenMetadata {
+            title: Some("title".to_string()),
+            description: Some("description".to_string()),
+            ..Default::default()
+        };
+        let receiver_id = "receiver.testnet".to_string();
+
+        contract.nft_mint(token_id.clone(), metadata.clone(), receiver_id.clone());
+        contract.nft_mint(token_id.clone(), metadata.clone(), receiver_id.clone());
+    }
+}

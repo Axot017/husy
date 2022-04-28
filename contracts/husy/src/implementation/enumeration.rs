5e81b157f09a3b37c433f0ae69143e90d2054ace
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2022-04-28 20:25:54 +0200

    add nft_total_supply implementation

--- contracts/husy/src/implementation/enumeration.rs
+++ contracts/husy/src/implementation/enumeration.rs
@@ -7,7 +7,9 @@ use crate::{
 
 #[near_bindgen]
 impl NFTEnumeration for HusyContract {
-    fn nft_total_supply(&self) {}
+    fn nft_total_supply(&self) -> U128 {
+        U128(self.meme_metadata_by_id.len().into())
+    }
 
     fn nft_tokens(&self, from_index: Option<U128>, limit: Option<u64>) {}
 
@@ -20,5 +22,64 @@ impl NFTEnumeration for HusyContract {
         limit: Option<u64>,
     ) -> Vec<MemeTokenView> {
         vec![]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::interface::ContractInit;
+    use crate::models::meme::MemeToken;
+    use crate::models::meme_metadata::MemeTokenMetadata;
+
+    use super::*;
+    use near_sdk::MockedBlockchain;
+    use near_sdk::{testing_env, VMContext};
+
+    fn get_context(predecessor_account_id: String, storage_usage: u64) -> VMContext {
+        VMContext {
+            current_account_id: "current.testnet".to_string(),
+            signer_account_id: "signer.testnet".to_string(),
+            signer_account_pk: vec![0, 1, 2],
+            predecessor_account_id,
+            input: vec![],
+            block_index: 0,
+            block_timestamp: 0,
+            account_balance: 0,
+            account_locked_balance: 0,
+            storage_usage,
+            attached_deposit: 0,
+            prepaid_gas: 10u64.pow(18),
+            random_seed: vec![0, 1, 2],
+            is_view: false,
+            output_data_receivers: vec![],
+            epoch_height: 19,
+        }
+    }
+
+    #[test]
+    fn test_nft_total_supply() {
+        let context = get_context("aaa.testnet".to_string(), 10000000);
+        testing_env!(context);
+        let mut contract = HusyContract::new_default("aaa.testnet".to_string());
+
+        let meme_token = MemeToken {
+            owner_id: "aaa.testnet".to_string(),
+        };
+        let meme_token_metadata = MemeTokenMetadata {
+            title: Some("title".to_string()),
+            description: Some("description".to_string()),
+            ..Default::default()
+        };
+        let ids = vec!["aaa".to_string(), "bbb".to_string(), "ccc".to_string()];
+        for id in &ids {
+            contract.memes_by_id.insert(id, &meme_token.clone());
+            contract
+                .meme_metadata_by_id
+                .insert(id, &meme_token_metadata.to_owned());
+        }
+
+        let result = contract.nft_total_supply();
+
+        assert_eq!(result, U128(ids.len() as u128))
     }
 }

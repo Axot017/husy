5908342cf2d623b92ffd9f6af5967cf33c67cd06
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2022-05-03 11:37:25 +0200

    add nft_revoke function implementation

--- contracts/husy/src/utils/payment.rs
+++ contracts/husy/src/utils/payment.rs
@@ -1,6 +1,4 @@
-use std::mem::size_of;
-
-use near_sdk::{env, AccountId, Balance, Promise};
+use near_sdk::{env, Balance, Promise};
 
 pub(crate) fn with_refund<F, R>(fun: F) -> R
 where
@@ -8,20 +6,25 @@ where
 {
     let initial_storage_usage = env::storage_usage();
     let result = fun();
-    let required_storage = env::storage_usage() - initial_storage_usage;
-    let required_cost = env::storage_byte_cost() * Balance::from(required_storage);
-    let attached_deposit = env::attached_deposit();
-
-    assert!(
-        required_cost <= attached_deposit,
-        "Must attach {} yoctoNEAR to cover storage",
-        required_cost,
-    );
-    let refund = attached_deposit - required_cost;
+    let final_storage_usage = env::storage_usage();
+    let refund = if initial_storage_usage > final_storage_usage {
+        let released_storage = initial_storage_usage - final_storage_usage;
+        env::storage_byte_cost() * Balance::from(released_storage)
+    } else {
+        let required_additional_storage = final_storage_usage - initial_storage_usage;
+        let required_cost = env::storage_byte_cost() * Balance::from(required_additional_storage);
+        let attached_deposit = env::attached_deposit();
+        assert!(
+            required_cost <= attached_deposit,
+            "Must attach {} yoctoNEAR to cover storage",
+            required_cost,
+        );
+        attached_deposit - required_cost
+    };
 
     if refund > 0 {
         Promise::new(env::predecessor_account_id()).transfer(refund);
     }
 
-    result 
+    result
 }

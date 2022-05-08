ba80af655c844d33b52da6f41ad47ce7fe34087f
Mateusz Ledwoń <mateusz.ledwon@iteo.com>
2022-05-08 20:04:27 +0200

    install app dependencies

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -1,4 +1,8 @@
-// window.ccxt = require('ccxt');
+window.walletSelector = require('@near-wallet-selector/core').default;
+window.setupNearWallet = require("@near-wallet-selector/near-wallet").setupNearWallet;
+window.setupSender =  require("@near-wallet-selector/sender").setupSender;
+window.setupMathWallet =  require("@near-wallet-selector/math-wallet").setupMathWallet;
+window.setupLedger =  require("@near-wallet-selector/ledger").setupLedger;
 
 import("./pkg").then(module => {
   module.run_app();
